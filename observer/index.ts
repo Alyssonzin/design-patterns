ea29a8e67e491f13cd5edc1db9c3d66ab04d5541
Alysson Pereira <alyssonempire84@gmail.com>
2026-01-15 23:46:42 -0300

    feat: adicionar comentários explicativos sobre a implementação do padrão Observer

--- observer/index.ts
+++ observer/index.ts
@@ -6,12 +6,19 @@ import { UserNotifier } from "./service/UserNotifier";
 
 const binanceApi = new BinanceApi();
 
+//Supõe que buscamos o preço atual do Bitcoin através de uma API externa
 const newPrice = binanceApi.getPrice();
 
+//A classe Bitcoin será nossa classe OBSERVADA (Subject)
 const bitcoin = new Bitcoin();
 
+/*
+  Aqui, adicionamos os observadores interessados na classe Bitcoin, que devem reagir à uma mudança no preço do Bitcoin.
+  Cada classe adicionada implementa `BitcoinPriceObserver` e reage ao update recebido do Subject
+*/
 bitcoin.addObserver(new NewsPlatform());
 bitcoin.addObserver(new BitcoinPriceLogger());
 bitcoin.addObserver(new UserNotifier());
 
+//Alterar o valor do Bitcoin (nosso evento de interesse) dispara internamente a função de notificação aos observadores
 bitcoin.setPrice(newPrice);
