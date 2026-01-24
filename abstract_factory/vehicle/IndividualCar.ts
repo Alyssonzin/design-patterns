61a17559502b3ccaa2541dbfa52fae95d0c9b0cf
Alysson Pereira <alyssonempire84@gmail.com>
2026-01-23 21:44:06 -0300

    feat: add abstract factory

---
+++ abstract_factory/vehicle/IndividualCar.ts
@@ -0,0 +1,12 @@
+import { Customer } from "../customer/Customer";
+import { Vehicle } from "./Vehicle";
+
+export class IndividualCar implements Vehicle {
+  constructor(public name: string, private readonly customer: Customer) {
+
+  }
+
+  pickUp(): void {
+    console.log(`${this.name} is picking up ${this.customer.name}`);
+  }
+}
